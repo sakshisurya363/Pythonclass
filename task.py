914cab6242222fcb97e41951208b46725ce47484
sakshisurya363 <sakshisuryawanshi2412@gmail.com>
2025-06-10 09:20:44 +0530

    Add files via upload

---
+++ task.py
@@ -0,0 +1,13 @@
+l=["Whatsapp","Facebook","Instagram","Twitter","linkedin"]
+l.remove("Facebook")
+print(l)
+
+# ['Whatsapp', 'Instagram', 'Twitter', 'linkedin']
+
+
+s=["Whatsapp","Facebook","Instagram","Twitter","linkedin"]
+print(s.pop())
+print(s)
+
+#linkedin
+#['Whatsapp', 'Facebook', 'Instagram', 'Twitter']
