04eb2d808793f9addb224584500e86950246e737
sakshisurya363 <sakshisuryawanshi2412@gmail.com>
2025-06-13 12:09:49 +0530

    Add files via upload

---
+++ Op_task.py
@@ -0,0 +1,67 @@
+# Addition of int
+a=25
+b=45
+print(a+b)
+
+# Substraction of int
+x=70
+y=30
+print(x-y)
+
+# Multilication of int
+x=70
+y=30
+print(x*y)
+
+# Addition of float
+a=30.5
+b=40.5
+print(a+b)
+
+# SUbstraction of float
+p=30.5
+q=10.4
+print(p-q)
+
+# Multiplication of float
+p=30.5
+q=10.4
+print(p*q)
+
+# Addition of two list
+l=[10,20,30,40]
+l1=[11,22,33,44]
+print(l+l1)
+
+# Multiplication of two list
+# l=[10,20,30,40]
+# l1=[11,22,33,44]      #TypeError: can't multiply sequence by non-int of type 'list'
+# print(l*l1)
+
+# Addition of two Tuple
+
+t=(12,13,14,15,)
+t1=(21,22,23,24,25,)
+print(t+t1)
+
+# Substraction of two tuple
+# t=(12,13,14,15,)
+# t1=(21,22,23,24,25,)   #TypeError: unsupported operand type(s) for -: 'tuple' and 'tuple'
+# print(t-t1)
+
+# Addition of two set
+
+# s={23,45,67,89}
+# s1={24,45,65,11}   #TypeError: unsupported operand type(s) for +: 'set' and 'set'
+# print(s+s1)
+
+# Substraction of two set
+
+s={23,45,67,89}
+s1={24,45,65,11}   
+print(s-s1)
+
+# Multiplication of two set
+s={23,45,67,89}
+s1={24,45,65,11}   #TypeError: unsupported operand type(s) for *: 'set' and 'set'
+print(s*s1)
